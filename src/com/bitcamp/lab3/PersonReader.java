f49744a0f781d9368a7c891c4ba88d218f8ac656
sanella <grcicsanela89@gmail.com>
2015-01-28 16:58:11 +0100

    Drugoo

--- src/com/bitcamp/lab3/PersonReader.java
+++ src/com/bitcamp/lab3/PersonReader.java
@@ -1,5 +1,4 @@
 package com.bitcamp.lab3;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
@@ -26,15 +25,26 @@ public class PersonReader {
 		Document xmldoc = docReader.parse(new File("./XML/people.xml"));
 		NodeList xmlPeople = xmldoc.getElementsByTagName("person");
 		LinkedList people = new LinkedList<Person>();
+		
 		for (int i = 0; i < xmlPeople.getLength(); i++) {
-
 			Node current = xmlPeople.item(i);
 
 			if (current instanceof Element) {
 				Element currentElement = (Element) current;
 				String name = currentElement.getAttribute("name");
 				String surname = currentElement.getAttribute("surname");
-				people.add(new Person(name, surname));
+				Person personTemp = new Person(name, surname) ;
+				NodeList xmlChildren = current.getChildNodes();
+				for(int j = 0; j<xmlChildren.getLength(); j++){
+					Node currentChild = xmlChildren.item(j);
+					if(currentChild instanceof Element){
+						Element currentChildElement = (Element) currentChild;
+						String childName = currentChildElement.getAttribute("name");
+						String childSurname = currentChildElement.getAttribute("surname");
+						personTemp.addChild(new Person(childName, childSurname));
+					}
+				}
+				people.add(personTemp);
 			}
 
 		}
